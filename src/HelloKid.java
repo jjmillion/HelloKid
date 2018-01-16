1d45d599821115c5221ea89f8e2b738723ef6846
Student <Student@LAB-101-44>
2018-01-16 18:21:13 +0700

    Revert to Hello Kid

--- src/HelloKid.java
+++ src/HelloKid.java
@@ -1,13 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-import java.util.Scanner;
-        
+/**
+ *
+ * @author Student
+ */
 public class HelloKid {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Input Your Name");
-        
-        String name = input.nextLine();
-        System.out.println("Hello " + name);
+     
+        System.out.println("Hello Kid");
         
     }
 }
