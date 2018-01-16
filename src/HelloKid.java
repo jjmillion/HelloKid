35f4bb55789f03d9d8b9988151a363381e7a812d
Student <Student@LAB-101-44>
2018-01-16 19:47:41 +0700

    Hello Input and Hello Name yang ke 2

--- src/HelloKid.java
+++ src/HelloKid.java
@@ -5,7 +5,7 @@ public class HelloKid {
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
-        System.out.println("Inputlah Nama Mu");
+        System.out.println("Inputlah Nama Mu Yang ke 2");
         
         String name = input.nextLine();
         System.out.println("Hello " + name);
