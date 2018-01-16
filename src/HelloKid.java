2516bc004ea1d0c69b573700144fd4e2c024913f
Javier Jonatan <javier.cipta@gmail.com>
2018-01-16 18:30:18 +0700

    Revert to Hello Input and Hello Name

--- src/HelloKid.java
+++ src/HelloKid.java
@@ -1,17 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Student
- */
+import java.util.Scanner;
+        
 public class HelloKid {
     public static void main(String[] args) {
-     
-        System.out.println("Hello Kid");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input Your Name");
+        
+        String name = input.nextLine();
+        System.out.println("Hello " + name);
         
     }
 }
